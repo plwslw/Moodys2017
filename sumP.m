68d3043c35ec7f204c37be029b9db6a220003cce
Phionx <shanjha.usa@gmail.com>
2017-02-26 16:38:29 -0500

    done

--- sumP.m
+++ sumP.m
@@ -1,8 +1,9 @@
 function out = sumP(monthlyData)
-out = zeros(1,numel(monthlyData));
-for i=1:1:numel(monthlyData);
-    out(i)=sum(monthlyData(1:i));
-    
-end
+% out = zeros(1,numel(monthlyData));
+% for i=1:1:numel(monthlyData);
+%     out(i)=sum(monthlyData(1:i));
+%     
+% end
+out = monthlyData;
 end
 
